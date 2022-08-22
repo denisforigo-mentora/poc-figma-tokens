f782dce2f2f9a73933b32b7a94ac83f42705064c
Denis Forigo <denisforigo@gmail.com>
2022-08-22 10:00:02 -0300

    initial commit

---
+++ src/style-dictionary-dist/variables.js
@@ -0,0 +1,9 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 22 Aug 2022 12:50:50 GMT
+ */
+
+export const ColorBasePrimary = "#61dafb";
+export const ColorBaseSecondary = "#fc7aa5";
+export const ColorText = "#666666";
+export const ColorBackground = "#eeeeee";
