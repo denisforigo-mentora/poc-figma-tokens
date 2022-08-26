82ab4bccdba23367f7267cdedea4fbd436b9da9d
Denis Forigo <denisforigo@gmail.com>
2022-08-26 01:23:15 -0300

    change color

--- src/style-dictionary-dist/variables.js
+++ src/style-dictionary-dist/variables.js
@@ -1,9 +1,12 @@
 /**
  * Do not edit directly
- * Generated on Mon, 22 Aug 2022 16:41:06 GMT
+ * Generated on Fri, 26 Aug 2022 04:19:21 GMT
  */
 
-export const ColorBasePrimary = "#61dafb";
-export const ColorBaseSecondary = "#fc7aa5";
-export const ColorText = "#666666";
 export const ColorBackground = "#eeeeee";
+export const ColorSm = "#000000";
+export const ColorMd = "#000000";
+export const ColorText = "#7e3b77";
+export const ColorCard = "#cccccc";
+export const ColorMedium = "#000000";
+export const ColorNormal = "#000000";
